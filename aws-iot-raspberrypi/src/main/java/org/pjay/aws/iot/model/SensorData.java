ffd9f8b8fb95ca45881ad9e647c4fd4fb66d9992
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-06-17 09:05:25 +0530

    Vijay - 2017/06/17 - AWS IoT examples

---
+++ aws-iot-raspberrypi/src/main/java/org/pjay/aws/iot/model/SensorData.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package org.pjay.aws.iot.model;
+
+/**
+ * @author Vijay Konduru
+ *
+ */
+public class SensorData {
+
+	private float temperature;
+	private float humidity;
+
+	public SensorData(float temperature, float humidity) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+	}
+
+	public float getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(float temperature) {
+		this.temperature = temperature;
+	}
+
+	public float getHumidity() {
+		return humidity;
+	}
+
+	public void setHumidity(float humidity) {
+		this.humidity = humidity;
+	}
+
+}
